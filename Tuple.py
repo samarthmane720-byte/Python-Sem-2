9c7d59bd70354821a9f337d94c57967b783b2ef3
samarthmane720-byte <samarthmane720@gmail.com>
2026-02-07 22:57:49 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,14 @@
+# Tuple Operations in Python
+t1 = (10,20,30,40)
+print("Tuple:",t1)
+print("First element:",t1[0])
+print("Last element:",t1[-1])
+
+print("2.Nested Tuple")
+nested_tuple = (1,2(3,4,5)6)
+print("Nested Tuple:",nested_tuple)
+print("Acess nested element:",
+      nested_tuple[2][1])
+
+
+
